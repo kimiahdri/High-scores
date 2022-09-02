f045bcb444f4898da390e3a20af00f65911cc156
kimi <kimi@DESKTOP-HLSO57E>
2022-09-02 11:00:21 +0430

    Add project files.

---
+++ High scores/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+
+
+
+
+using System.Collections.Generic;
+
+
+
+
+
+using System.Linq;
+
+
+
+
+
+
+
+
+
+
+public class HighScores
+
+
+
+
+
+{
+
+
+
+
+
+    private List<int> scores { get; set; }
+
+
+
+
+
+
+
+
+
+
+    public HighScores(List<int> list)
+
+
+
+
+
+    {
+
+
+
+
+
+        scores = list;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public List<int> Scores()
+
+
+
+
+
+    {
+
+
+
+
+
+        return scores;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public int Latest()
+
+
+
+
+
+    {
+
+
+
+
+
+        return scores.Last();
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public int PersonalBest()
+
+
+
+
+
+    {
+
+
+
+
+
+        return scores.Max();
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public List<int> PersonalTopThree()
+
+
+
+
+
+    {
+
+
+
+
+
+        return (from i in scores orderby i descending select i).Take(3).ToList();
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
